5cd8e5ec66d5110b1f2c41628e76ee7a3e8ed95d
daniel <daniel.gilyadov@gmail.com>
2018-03-16 18:40:18 +0200

    Add winner

--- script.js
+++ script.js
@@ -1,29 +1,86 @@
 
 import $ from 'jquery';
 
+const PLAYER = {
+    X: 'X',
+    O: 'O'
+}
+const cells = document.querySelectorAll('.cell');
+
+const X_BACKGROUND = 'url("x.png")';
+const O_BACKGROUND = 'url("o.png")';
+
+const X_MESSAGE = 'Player X won!';
+const O_MESSAGE = 'Player O won!';
+
+
 let isX = true;
 
-const printX = () => {
-    $(this).css('background-image', 'url(x.png)')
+const printX = (element) => {
+    $(element).css('background-image', 'url("x.png")')
+    console.log("x")
 }
 
-const printO = () => {
-    $(this).css('background-image', 'url(o.png)')
+const printO = (element) => {
+    $(element).css('background-image', 'url("o.png")')
 }
 
-$(".cell").click(function() {
-    if($(this).css('background-image') !== 'none') { // Checks if already clicked before
+const checkRow = (cell1, cell2, cell3, player) => {
+    let img, message;
+    if (player === PLAYER.X) {
+        img = X_BACKGROUND;
+        message = X_MESSAGE;
+    } else {
+        img = O_BACKGROUND;
+        message = O_MESSAGE;
+    }
+    if(cells[cell1].style.backgroundImage === img && 
+       cells[cell2].style.backgroundImage === img && 
+       cells[cell3].style.backgroundImage === img) {
+        alert(message)
+        location.reload()
+    }
+}
+
+const winX = () => {
+    checkRow(0, 1, 2, PLAYER.X);
+    checkRow(3, 4, 5, PLAYER.X);
+    checkRow(6, 7, 8, PLAYER.X);
+    checkRow(0, 3, 6, PLAYER.X);
+    checkRow(1, 4, 7, PLAYER.X);
+    checkRow(2, 5, 8, PLAYER.X);
+    checkRow(0, 4, 8, PLAYER.X);
+    checkRow(2, 4, 6, PLAYER.X);
+}
+
+const winO = () => {
+    checkRow(0, 1, 2, PLAYER.O);
+    checkRow(3, 4, 5, PLAYER.O);
+    checkRow(6, 7, 8, PLAYER.O);
+    checkRow(0, 3, 6, PLAYER.O);
+    checkRow(1, 4, 7, PLAYER.O);
+    checkRow(2, 5, 8, PLAYER.O);
+    checkRow(0, 4, 8, PLAYER.O);
+    checkRow(2, 4, 6, PLAYER.O);
+}
+
+$(".cell").click((e) => {
+    if($(e.target).css('background-image') !== 'none') { // Checks if already clicked before
         return;
     }
+    console.log("cellClicked")
     if(isX === true) {
-        printX()
+        printX(e.target)
         isX = false
+        winX()
     } else {
-        printO()
+        printO(e.target)
         isX = true
+        winO()
     }
+    
 })
 
-$("#resetbtn").click(function() {
+$("#resetbtn").click(() => {
     location.reload() // Reset the game
 })
